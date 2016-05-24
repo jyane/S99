3a90f97ff1b48af7c3e6643799a0de4e5e84a67c
jyane <jyane.kstm@gmail.com>
2016-05-24 23:37:20 +0900

    added test 7

---
+++ src/test/scala/jp/jyane/s99/P07Test.scala
@@ -0,0 +1,15 @@
+package jp.jyane.s99
+
+import org.scalatest.{DiagrammedAssertions, FunSpec}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+class P07Test extends FunSpec with DiagrammedAssertions with GeneratorDrivenPropertyChecks {
+  describe("P07") {
+    it ("should return that flattened list") {
+      assert(P07.flatten(List(List(1, 1), 2, List(3, List(5, 8)))) ==
+        List(1, 1, 2, 3, 5, 8)
+      )
+    }
+  }
+}
+
